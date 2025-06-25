3dc94f1e19529fd03da9db106bdac08145449d13
Martin Egor Stoyanov <68874963+martinestoyanov@users.noreply.github.com>
2025-06-25 08:12:44 -0700

    Add files via upload

---
+++ LENOVO/TB132FU/AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_TB132FU.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_TB132FU-user \
+    omni_TB132FU-userdebug \
+    omni_TB132FU-eng
+
